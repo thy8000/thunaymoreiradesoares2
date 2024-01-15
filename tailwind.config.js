60e7d8b2aef6dccea6986975e51719d4e29d743b
Thunay <thunaymoreiradesoares@gmail.com>
2024-01-15 20:56:38 -0300

    🎉

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      black: colors.black,
+    }
+  },
+  plugins: [],
+}
+
