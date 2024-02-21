c389c43b1a84148cb9759bcbadda3c9e66c51c93
Thunay <thunaymoreiradesoares@gmail.com>
2024-02-20 21:42:21 -0300

    💄 Footer e Header

--- assets/js/all.js
+++ assets/js/all.js
@@ -66,6 +66,53 @@ document.addEventListener("alpine:init", () => {
     ],
   }));
 
+  Alpine.data("skills", () => ({
+    skillsList: [
+      {
+        ID: 1,
+        name: "HTML5",
+        percent: 95,
+        level: "Avançado",
+      },
+      {
+        ID: 2,
+        name: "CSS3",
+        percent: 85,
+        level: "Avançado",
+      },
+      {
+        ID: 3,
+        name: "JavaScript",
+        percent: 88,
+        level: "Avançado",
+      },
+      {
+        ID: 4,
+        name: "PHP",
+        percent: 93,
+        level: "Avançado",
+      },
+      {
+        ID: 5,
+        name: "Wordpress",
+        percent: 90,
+        level: "Avançado",
+      },
+      {
+        ID: 6,
+        name: "SASS",
+        percent: 60,
+        level: "Intermediário",
+      },
+      {
+        ID: 2,
+        name: "jQuery",
+        percent: 70,
+        level: "Intermediário",
+      },
+    ],
+  }));
+
   Alpine.data("projects", () => ({
     tab: "all",
     projects: [
@@ -241,6 +288,22 @@ document.addEventListener("alpine:init", () => {
         link2: {
           name: "Github",
           link: "https://github.com/thy8000/Infomarket",
+        },
+      },
+      {
+        ID: 12,
+        type: "others",
+        image: "../thunaymoreiradesoares2/assets/images/book-finder.png",
+        title: "Book Finder",
+        description:
+          "Um aplicativo desktop feito com Vite, Vue.js e TailwindCSS para um projeto pessoal.",
+        link1: {
+          name: "Github",
+          link: "https://github.com/thy8000/book-finder-app",
+        },
+        link2: {
+          name: "",
+          link: "",
         },
       },
     ],
