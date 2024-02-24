80b136b01f523671d286aefc092e472fa62df8f9
Thunay <thunaymoreiradesoares@gmail.com>
2024-02-24 16:22:53 -0300

    💄 Header

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,6 +3,43 @@
  */
 
 document.addEventListener("alpine:init", () => {
+  Alpine.data("header", () => ({
+    menuItems: [
+      {
+        ID: 1,
+        name: "Sobre mim",
+        element: document.querySelector('#about-me'),
+      },
+      {
+        ID: 2,
+        name: "Serviços",
+        element: document.querySelector('#services'),
+      },
+      {
+        ID: 3,
+        name: "Habilidades",
+        element: document.querySelector('#skills'),
+      },
+      {
+        ID: 4,
+        name: "Experiência Profissional",
+        element: document.querySelector('#experience'),
+      },
+      {
+        ID: 5,
+        name: "Projetos",
+        element: document.querySelector('#projects'),
+      },
+    ],
+    displayMenu: false,
+
+    goToElement(element) {
+      this.displayMenu = false;
+
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }));
+
   Alpine.data("hero", () => ({
     socials: [
       {
